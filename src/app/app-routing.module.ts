a0862a21987395a94111fa97c844e0e64206d369
Djibril Ba <djibril15.ba@ucad.edu.sn>
2023-06-28 00:33:54 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProduitsComponent } from './produits/produits.component';
+import { AddProduitComponent } from './add-produit/add-produit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'produits', component: ProduitsComponent },
+  {path: 'add-produit', component: AddProduitComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
