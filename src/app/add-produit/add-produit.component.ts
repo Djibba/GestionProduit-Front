892c7469c611c0a4efb3844575c7131450ac1d71
Djibril Ba <djibril15.ba@ucad.edu.sn>
2023-07-03 15:00:39 +0000

    Ajout des service d'ajout et d'affichage

--- src/app/add-produit/add-produit.component.ts
+++ src/app/add-produit/add-produit.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produit } from './../model/produit.model';
+import { ProduitService } from './../services/produit.service';
 
 @Component({
   selector: 'app-add-produit',
   templateUrl: './add-produit.component.html',
   styleUrls: ['./add-produit.component.css']
 })
-export class AddProduitComponent {
+export class AddProduitComponent implements OnInit {
+
+    newProduit = new Produit();
+
+    constructor(private produitService: ProduitService) { }
+
+    ngOnInit(): void {
+    }
+
+    addProduit() {
+      //console.log(this.newProduit);
+      // this.newProduit.dateCreation = new Date();
+      // this.newProduit.idProduit = this.produits.length + 1;
+      // this.produits.push(this.newProduit);
+      // this.newProduit = new Produit();
+      this.produitService.ajouterProduit(this.newProduit);
+    }
 
 }
--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Produit } from './../model/produit.model';
+import { ProduitService } from './../services/produit.service';
 
 @Component({
   selector: 'app-produits',
   templateUrl: './produits.component.html',
-  styleUrls: ['./produits.component.css']
+  styleUrls: ['./produits.component.css'],
 })
 export class ProduitsComponent implements OnInit {
+  produits: Produit[];
 
-  produits: string[];
-
-  constructor() {
-    this.produits = ['PC DELL', 'Imprimante Epson', 'Tablette Samsung'];
-   }
-
-  ngOnInit(): void {
+  constructor(private produitService: ProduitService) {
+    this.produits = produitService.listeProduits();
   }
 
+  ngOnInit(): void {}
 }
