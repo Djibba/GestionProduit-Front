7688211374fad1ed53cc565c939093170e8b264e
Djibril Ba <djibril15.ba@ucad.edu.sn>
2023-08-17 22:22:26 +0000

    creation du guard

---
+++ src/app/guards/produit.guard.ts
@@ -0,0 +1,20 @@
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthService } from './../services/auth.service';
+import { Observable } from 'rxjs';
+
+export class ProduitGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+      if(this.authService.isAdmin()){
+        return true;
+      }else{
+        this.router.navigate(['/login']);
+        return false;
+      }
+  }
+
+  }
+
+};
