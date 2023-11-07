951597bc9831307bc3d1093732b455f41dd0e83e
Djibril Ba <djibril15.ba@ucad.edu.sn>
2023-11-07 15:31:15 +0000

    updateImage ok

--- src/app/update-produit/update-produit.component.ts
+++ src/app/update-produit/update-produit.component.ts
@@ -16,7 +16,7 @@ export class UpdateProduitComponent implements OnInit {
   updatedCatId!: number;
   myImage!: string;
   uploadedImage!: File;
-    imagePath!: any;
+  isImageUpdated: Boolean = false;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -35,29 +35,46 @@ export class UpdateProduitComponent implements OnInit {
         this.updatedCatId = this.currentProduit.categorie!.idCat;
 
         this.produitService
-          .loadImage(this.currentProduit.image.idImage).subscribe((img: Image) => {
+          .loadImage(this.currentProduit.image.idImage)
+          .subscribe((img: Image) => {
             this.myImage = 'data:' + img.typeImage + ';base64,' + img.image;
-          }
-        );
+          });
       });
-
-
-
   }
 
   updateProduit() {
-    this.currentProduit.categorie = this.categories.find(cat => cat.idCat = this.updatedCatId)!;
-    this.produitService.updateProduit(this.currentProduit).subscribe((prod) => {
-      this.router.navigate(['produits']);
-    });
+    this.currentProduit.categorie = this.categories.find(
+      (cat) => (cat.idCat = this.updatedCatId)
+    )!;
+
+    if (this.isImageUpdated) {
+      this.produitService
+        .uploadImage(this.uploadedImage, this.uploadedImage.name)
+        .subscribe((img: Image) => {
+          this.currentProduit.image = img;
+          this.produitService.updateProduit(this.currentProduit).subscribe((prod) => {
+            this.router.navigate(['produits']);
+          }
+          );
+        });
+    } else {
+      this.produitService.updateProduit(this.currentProduit).subscribe((prod) => {
+        this.router.navigate(['produits']);
+      });
+    }
+
+
   }
 
-  // onImageUpload(event: any) {
-  //   this.uploadedImage = event.target.files[0];
-  //   var reader = new FileReader();
-  //   reader.readAsDataURL(this.uploadedImage);
-  //   reader.onload = (_event) => {
-  //     this.imagePath = reader.result;
-  //   }
-  // }
+  onImageUpload(event: any) {
+    if (event.target.files && event.target.files.length) {
+      this.uploadedImage = event.target.files[0];
+      this.isImageUpdated = true;
+      const reader = new FileReader();
+      reader.readAsDataURL(this.uploadedImage);
+      reader.onload = () => {
+        this.myImage = reader.result as string;
+      };
+    }
+  }
 }
